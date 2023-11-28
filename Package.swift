5071b6ab4f4bddbb0d59890730f9ee3be9b248d8
hj.kwon <hj.kwon@toss.im>
2023-11-28 18:35:57 +0900

    Change Swift Version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TossSDK-iOS",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
